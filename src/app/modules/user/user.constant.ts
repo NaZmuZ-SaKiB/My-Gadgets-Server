dd618a470c99fbf30a907a3b6937652ae08fd71b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-25 15:30:02 +0600

    chore: Update user roles constant

--- src/app/modules/user/user.constant.ts
+++ src/app/modules/user/user.constant.ts
@@ -1,11 +1,9 @@
-import { TUserRole } from "./user.type";
+import { TUserRole } from './user.type';
 
-export const userRoles: TUserRole[] = ["super_admin", "admin", "user"];
+export const userRoles: TUserRole[] = ['super_admin', 'admin', 'user'];
 
-export const USER_ROLE: {
-  [key: string]: TUserRole;
-} = {
-  SUPER_ADMIN: "super_admin",
-  ADMIN: "admin",
-  USER: "user",
+export const USER_ROLE = {
+  SUPER_ADMIN: 'super_admin',
+  ADMIN: 'admin',
+  USER: 'user',
 } as const;
