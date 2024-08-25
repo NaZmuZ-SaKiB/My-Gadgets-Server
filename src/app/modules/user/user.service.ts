71de90e6d4298601e538a1521a47dd3f5a5f0c81
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-25 15:20:14 +0600

    feat: Add sign up and sign in functionality to user service

---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,69 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { USER_ROLE } from './user.constant';
+import User from './user.model';
+import { TUser } from './user.type';
+import { jwtHelpers } from '../../utils/jwtHelpers';
+import config from '../../config';
+import bcrypt from 'bcrypt';
+
+const signUp = async (payload: TUser) => {
+  if (payload.role !== USER_ROLE.USER) {
+    throw new AppError(
+      httpStatus.FORBIDDEN,
+      `You are not allowed to create this role: "${payload.role}"`,
+    );
+  }
+
+  const user = await User.create(payload);
+
+  const token = await jwtHelpers.generateToken(
+    {
+      _id: `${user._id}`,
+      email: user.email,
+      role: user.role,
+    },
+    `${config.jwt_access_secret}`,
+    `${config.jwt_access_expires_in}`,
+  );
+
+  return { user, token };
+};
+
+const signIn = async (payload: { email: string; password: string }) => {
+  const user = await User.findOne({
+    email: payload.email,
+  }).select('+password');
+
+  if (!user) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Invalid email or password');
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(
+    payload.password,
+    user.password,
+  );
+
+  if (!isPasswordCorrect) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Invalid email or password');
+  }
+
+  const token = await jwtHelpers.generateToken(
+    {
+      _id: `${user._id}`,
+      email: user.email,
+      role: user.role,
+    },
+    `${config.jwt_access_secret}`,
+    `${config.jwt_access_expires_in}`,
+  );
+
+  user.password = '';
+
+  return { user, token };
+};
+
+export const UserService = {
+  signUp,
+  signIn,
+};
