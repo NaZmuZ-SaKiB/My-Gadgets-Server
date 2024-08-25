cada4c362858778bdc51813bf93433ecc99acc4e
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-25 20:33:50 +0600

    refactor: Update validateRequest middleware to simplify request body parsing

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,13 +1,11 @@
-import { RequestHandler } from "express";
-import { AnyZodObject } from "zod";
+import { RequestHandler } from 'express';
+import { AnyZodObject } from 'zod';
 
 const validateRequest = (schema: AnyZodObject) =>
   (async (req, res, next) => {
     try {
-      const parse = await schema.parseAsync({
-        body: req.body,
-      });
-      req.body = parse.body;
+      const parse = await schema.parseAsync(req.body);
+      req.body = parse;
 
       next();
     } catch (err) {
