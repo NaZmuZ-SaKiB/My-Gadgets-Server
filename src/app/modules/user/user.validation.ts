a9f076e66cd3266fc4147b396704904c326afd24
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-25 15:25:27 +0600

    chore: Update user validation with sign up and sign in schemas

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,46 @@
+import { z } from 'zod';
+
+const signUp = z.object({
+  name: z
+    .string({
+      required_error: 'Name is required',
+      invalid_type_error: 'Name must be a string',
+    })
+    .min(3, 'Name must be at least 3 characters long')
+    .max(50, 'Name must be at most 50 characters long'),
+
+  email: z
+    .string({
+      required_error: 'Email is required',
+      invalid_type_error: 'Email must be a string',
+    })
+    .email('Invalid email format'),
+
+  password: z
+    .string({
+      required_error: 'Password is required',
+      invalid_type_error: 'Password must be a string',
+    })
+    .min(6, 'Password must be at least 6 characters long'),
+});
+
+const signIn = z.object({
+  email: z
+    .string({
+      required_error: 'Email is required',
+      invalid_type_error: 'Email must be a string',
+    })
+    .email('Invalid email format'),
+
+  password: z
+    .string({
+      required_error: 'Password is required',
+      invalid_type_error: 'Password must be a string',
+    })
+    .min(6, 'Password must be at least 6 characters long'),
+});
+
+export const UserValidation = {
+  signUp,
+  signIn,
+};
