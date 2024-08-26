8ba53511b4fe28cb590b7b2267fbc1524c2ceebf
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-26 14:06:50 +0600

    feat: Add featured, showOnTopMenu, and image fields to category

--- src/app/modules/category/category.model.ts
+++ src/app/modules/category/category.model.ts
@@ -14,6 +14,18 @@ const categorySchema = new Schema<TCategory>(
       required: true,
       trim: true,
     },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    showOnTopMenu: {
+      type: Boolean,
+      default: false,
+    },
+    image: {
+      type: Schema.Types.ObjectId,
+      ref: 'Media',
+    },
 
     updatedBy: {
       type: Schema.Types.ObjectId,
--- src/app/modules/category/category.type.ts
+++ src/app/modules/category/category.type.ts
@@ -3,6 +3,9 @@ import { Types } from 'mongoose';
 export type TCategory = {
   name: string;
   label: string;
+  featured: boolean;
+  showOnTopMenu: boolean;
+  image: Types.ObjectId;
 
   createdAt: Date;
   updatedAt: Date;
