86de819fd655740473a9c4f3a1d6a4c89aa25014
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-28 13:02:46 +0600

    feat: Make 'image' field optional in brand type

--- src/app/modules/brand/brand.type.ts
+++ src/app/modules/brand/brand.type.ts
@@ -3,7 +3,7 @@ import { Types } from 'mongoose';
 export type TBrand = {
   name: string;
   featured: boolean;
-  image: Types.ObjectId;
+  image?: Types.ObjectId;
 
   createdAt: Date;
   updatedAt: Date;
