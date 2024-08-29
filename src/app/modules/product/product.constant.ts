d46a745b55475cdc3f1ce2c518fc96a2c9055fc7
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-29 13:36:28 +0600

    refactor: Remove unused fields and constants in product module

---
+++ src/app/modules/product/product.constant.ts
@@ -0,0 +1,55 @@
+import {
+  TChargingPort,
+  TCompatibility,
+  TConnectivity,
+  TOperatingSystem,
+  TPowerSource,
+} from './product.type';
+
+export const operatingSystems: TOperatingSystem[] = [
+  'android',
+  'ios',
+  'linux',
+  'macos',
+  'windows',
+];
+
+export const chargingPorts: TChargingPort[] = [
+  'usb-c',
+  'lightning',
+  'micro-usb',
+];
+
+export const connectivities: TConnectivity[] = ['bluetooth', 'wifi'];
+
+export const powerSources: TPowerSource[] = ['battery', 'plug-in'];
+
+export const compatibilities: TCompatibility[] = [
+  ...operatingSystems,
+  'iphone',
+  'android-phone',
+  'laptop',
+  'macbook',
+];
+
+export const fieldsToRemove = [
+  'search',
+  'minPrice',
+  'maxPrice',
+  'releasedBefore',
+  'releasedAfter',
+  'connectivity',
+  'compatibility',
+  'os',
+  'minCamera',
+  'maxCamera',
+  'minWeight',
+  'maxWeight',
+  'minDisplaySize',
+  'maxDisplaySize',
+  'minQuantity',
+  'maxQuantity',
+  'sort',
+  'page',
+  'limit',
+];
