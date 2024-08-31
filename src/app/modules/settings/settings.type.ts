18d53903ef51389005f2c66e6b4bbcf1dae21414
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-31 18:53:22 +0600

    feat: Add settings type definitions

---
+++ src/app/modules/settings/settings.type.ts
@@ -0,0 +1,34 @@
+import { Types } from 'mongoose';
+
+export type THomepageSettings = {
+  sliderImages: Types.ObjectId[];
+  bannerImage1: Types.ObjectId;
+  bannerImage2: Types.ObjectId;
+  bannerImage3: Types.ObjectId;
+  featuredCategories: Types.ObjectId[];
+  featuredBrands: Types.ObjectId[];
+  popularProducts: Types.ObjectId[];
+  featuredProducts: {
+    banner?: Types.ObjectId;
+    products: Types.ObjectId[];
+  }[];
+  flashSale: {
+    product: Types.ObjectId;
+    endDate: Date;
+  }[];
+  topSellingProducts: Types.ObjectId[];
+  trendingProducts: Types.ObjectId[];
+};
+
+export type TFooterSettings = {
+  slogan: string;
+  email: string;
+  hours: string;
+  contact: string;
+  copyright: string;
+};
+
+export type TSettings = {
+  homepage: THomepageSettings;
+  footer: TFooterSettings;
+};
