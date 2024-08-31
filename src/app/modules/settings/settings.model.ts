b6cfeba1bcaa2881c3303b5df911ac2eff773438
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-31 18:53:34 +0600

    feat: Add settings model with homepage and footer schemas

---
+++ src/app/modules/settings/settings.model.ts
@@ -0,0 +1,71 @@
+import { model, Schema } from 'mongoose';
+import { TFooterSettings, THomepageSettings, TSettings } from './settings.type';
+
+const homepageSettingsSchema = new Schema<THomepageSettings>(
+  {
+    sliderImages: [{ type: Schema.Types.ObjectId, ref: 'Media' }],
+    bannerImage1: { type: Schema.Types.ObjectId, ref: 'Media' },
+    bannerImage2: { type: Schema.Types.ObjectId, ref: 'Media' },
+    bannerImage3: { type: Schema.Types.ObjectId, ref: 'Media' },
+    featuredCategories: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
+    featuredBrands: [{ type: Schema.Types.ObjectId, ref: 'Brand' }],
+    popularProducts: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+    featuredProducts: [
+      {
+        banner: { type: Schema.Types.ObjectId, ref: 'Media' },
+        products: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+      },
+    ],
+    flashSale: [
+      {
+        product: { type: Schema.Types.ObjectId, ref: 'Product' },
+        endDate: {
+          type: Date,
+          required: true,
+        },
+      },
+    ],
+    topSellingProducts: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+    trendingProducts: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+  },
+  {
+    _id: false,
+  },
+);
+
+const footerSettingsSchema = new Schema<TFooterSettings>(
+  {
+    slogan: {
+      type: String,
+      required: true,
+      default: 'Your slogan here',
+    },
+    email: {
+      type: String,
+      required: true,
+      default: 'example@email.com',
+    },
+    hours: {
+      type: String,
+      required: true,
+      default: '10:00AM - 8:00PM',
+    },
+    contact: {
+      type: String,
+      required: true,
+      default: '123-456-7890',
+    },
+  },
+  {
+    _id: false,
+  },
+);
+
+const settingsSchema = new Schema<TSettings>({
+  homepage: homepageSettingsSchema,
+  footer: footerSettingsSchema,
+});
+
+const Settings = model<TSettings>('Settings', settingsSchema);
+
+export default Settings;
