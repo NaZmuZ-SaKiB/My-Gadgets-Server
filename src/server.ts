f6e0bc4dcb6c2fc4e47cd4c150c292ba62f564b0
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-08-31 19:00:19 +0600

    feat: Seed default settings if not present

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,14 @@ import app from './app';
 
 import User from './app/modules/user/user.model';
 import { USER_ROLE } from './app/modules/user/user.constant';
+import Settings from './app/modules/settings/settings.model';
+import { defaultSettings } from './app/modules/settings/settings.constant';
 
 const main = async () => {
   try {
     await mongoose.connect(config.database_url as string);
 
-    // seed admin
+    // seed super admin
     const isAdmin = await User.findOne({ role: USER_ROLE.SUPER_ADMIN }).select(
       '_id',
     );
@@ -22,6 +24,13 @@ const main = async () => {
         role: USER_ROLE.SUPER_ADMIN,
         password: 'superman',
       });
+    }
+
+    // seed settings
+    const settings = await Settings.findOne();
+
+    if (!settings) {
+      await Settings.create(defaultSettings);
     }
 
     app.listen(config.port, () => {
