62df0985f80efef8d6bf13bdd28cd7950b9e802f
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-03 22:05:07 +0600

    feat: Update homepage and footer settings validation

--- src/app/modules/settings/settings.validation.ts
+++ src/app/modules/settings/settings.validation.ts
@@ -1,43 +1,47 @@
 import { z } from 'zod';
 
 const update = z.object({
-  homepage: z.object({
-    sliderImages: z.array(z.string()).optional(),
-    bannerImage1: z.string().optional(),
-    bannerImage2: z.string().optional(),
-    bannerImage3: z.string().optional(),
-    featuredCategories: z.array(z.string()).optional(),
-    featuredBrands: z.array(z.string()).optional(),
-    popularProducts: z.array(z.string()).optional(),
-    featuredProducts: z
-      .array(
-        z.object({
-          banner: z.string().optional(),
-          products: z.array(z.string()),
-        }),
-      )
-      .optional(),
-    flashSale: z
-      .array(
-        z.object({
-          product: z.string(),
-          endDate: z.string(),
-        }),
-      )
-      .optional(),
+  homepage: z
+    .object({
+      sliderImages: z.array(z.string()).optional(),
+      bannerImage1: z.string().optional(),
+      bannerImage2: z.string().optional(),
+      bannerImage3: z.string().optional(),
+      featuredCategories: z.array(z.string()).optional(),
+      featuredBrands: z.array(z.string()).optional(),
+      popularProducts: z.array(z.string()).optional(),
+      featuredProducts: z
+        .array(
+          z.object({
+            banner: z.string().optional(),
+            products: z.array(z.string()),
+          }),
+        )
+        .optional(),
+      flashSale: z
+        .array(
+          z.object({
+            product: z.string(),
+            endDate: z.date(),
+          }),
+        )
+        .optional(),
 
-    topSellingProducts: z.array(z.string()).optional(),
-    trendingProducts: z.array(z.string()).optional(),
-    description: z.string().optional(),
-  }),
+      topSellingProducts: z.array(z.string()).optional(),
+      trendingProducts: z.array(z.string()).optional(),
+      description: z.string().optional(),
+    })
+    .optional(),
 
-  footer: z.object({
-    slogan: z.string().optional(),
-    email: z.string().optional(),
-    hours: z.string().optional(),
-    contact: z.string().optional(),
-    copyright: z.string().optional(),
-  }),
+  footer: z
+    .object({
+      slogan: z.string().optional(),
+      email: z.string().optional(),
+      hours: z.string().optional(),
+      contact: z.string().optional(),
+      copyright: z.string().optional(),
+    })
+    .optional(),
 });
 
 export const SettingsValidation = {
