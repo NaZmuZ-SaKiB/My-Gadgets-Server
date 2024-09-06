446309d51158822c85411a087f01d5bfbedc90a8
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-06 23:23:44 +0600

    feat: Add status field to TReview type

--- src/app/modules/review/review.type.ts
+++ src/app/modules/review/review.type.ts
@@ -1,10 +1,13 @@
 import { Types } from 'mongoose';
 
+export type TReviewStatus = 'pending' | 'approved' | 'rejected';
+
 export type TReview = {
   product: Types.ObjectId;
   user: Types.ObjectId;
   rating: number;
   comment: string;
+  status: TReviewStatus;
 
   createdAt: Date;
   updatedAt: Date;
