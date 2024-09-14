328a5e089448ae8ead6acd0bd3d003e48860a087
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-14 22:31:23 +0600

    refactor: Add getAll, getById, and remove functions to shippingAddress controller

--- src/app/modules/shippingAddress/shippingAddress.controller.ts
+++ src/app/modules/shippingAddress/shippingAddress.controller.ts
@@ -28,3 +28,45 @@ const update = catchAsync(async (req, res) => {
     data: result,
   });
 });
+
+const getAll = catchAsync(async (req, res) => {
+  const result = await ShippingAddressService.getAll(req.user?._id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    data: result,
+  });
+});
+
+const getById = catchAsync(async (req, res) => {
+  const result = await ShippingAddressService.getById(req.params.id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    data: result,
+  });
+});
+
+const remove = catchAsync(async (req, res) => {
+  const result = await ShippingAddressService.remove(
+    req.user?._id,
+    req.params.id,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.NO_CONTENT,
+    success: true,
+    message: 'Shipping address removed successfully.',
+    data: result,
+  });
+});
+
+export const ShippingAddressController = {
+  create,
+  update,
+  getAll,
+  getById,
+  remove,
+};
