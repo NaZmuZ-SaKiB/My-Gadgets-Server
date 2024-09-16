bc147ba114192e7d99160e9e35bf9f0a9f3983cf
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-16 21:39:54 +0600

    feat: Add TOrder type for order module

---
+++ src/app/modules/order/order.type.ts
@@ -0,0 +1,32 @@
+import { Types } from 'mongoose';
+
+export type TOrderItem = {
+  name: string;
+  slug: string;
+  image: string;
+  price: number;
+  quantity: number;
+  product: Types.ObjectId;
+};
+
+export type TPaymentMethod = 'cash' | 'stripe';
+
+export type TOrderStatus = 'pending' | 'processing' | 'completed' | 'cancelled';
+
+export type TOrder = {
+  user: Types.ObjectId;
+  orderItems: TOrderItem[];
+  shippingAddress: Types.ObjectId;
+  paymentMethod: TPaymentMethod;
+  paymentResult?: string;
+  transactionId?: string;
+  shippingCharge: number;
+  totalPrice: number;
+  isPaid: boolean;
+  paidAt: Date;
+  completedAt: Date;
+  status: TOrderStatus;
+
+  createdAt: Date;
+  updatedAt: Date;
+};
