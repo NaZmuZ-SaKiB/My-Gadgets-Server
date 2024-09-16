ada67c02a7f2c45c9c1f038295a4a9fef20f9e9e
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-16 21:53:53 +0600

    feat: Add TOrder model for order module

---
+++ src/app/modules/order/order.model.ts
@@ -0,0 +1,83 @@
+import { Schema } from 'mongoose';
+import { TOrder, TOrderItem } from './order.type';
+import { ORDER_STATUS, orderStatuses, paymentMethods } from './order.constant';
+
+const orderItemSchema = new Schema<TOrderItem>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+  },
+  {
+    _id: false,
+  },
+);
+
+const orderSchema = new Schema<TOrder>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    orderItems: [orderItemSchema],
+    shippingAddress: {
+      type: Schema.Types.ObjectId,
+      ref: 'ShippingAddress',
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      enum: paymentMethods,
+      required: true,
+    },
+    paymentResult: String,
+    transactionId: String,
+    shippingCharge: {
+      type: Number,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: Date,
+    completedAt: Date,
+    status: {
+      type: String,
+      enum: orderStatuses,
+      required: true,
+      default: ORDER_STATUS.PENDING,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
