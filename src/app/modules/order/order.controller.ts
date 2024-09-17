6fb62114917546dfb82ec547d54992cf7d32b370
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-17 11:55:45 +0600

    feat: Add order update controller

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -13,3 +13,14 @@ const create = catchAsync(async (req, res) => {
     data: result,
   });
 });
+
+const update = catchAsync(async (req, res) => {
+  const result = await OrderServices.update(req?.params?.id, req.body);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Order Updated.',
+    data: result,
+  });
+});
