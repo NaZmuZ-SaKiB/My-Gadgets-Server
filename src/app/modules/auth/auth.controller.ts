444a57c89e08f285737f2f72fc8f5cc66fe1f86b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-09-30 20:15:27 +0600

    refactor: Add currentUser function to AuthService

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,6 +3,17 @@ import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { AuthService } from './auth.service';
 
+const currentUser = catchAsync(async (req, res) => {
+  const result = await AuthService.currentUser(req.user._id);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Current user fetched successfully.',
+    data: result,
+  });
+});
+
 const signUp = catchAsync(async (req, res) => {
   const result = await AuthService.signUp(req.body);
 
@@ -26,6 +37,7 @@ const signIn = catchAsync(async (req, res) => {
 });
 
 export const AuthController = {
+  currentUser,
   signUp,
   signIn,
 };
