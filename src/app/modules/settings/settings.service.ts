4df29944877993174a043b0f2a226be3d21f199c
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-07 16:56:58 +0600

    feat: Refactor settings retrieval to support category and footer types

--- src/app/modules/settings/settings.service.ts
+++ src/app/modules/settings/settings.service.ts
@@ -1,62 +1,88 @@
 import Settings from './settings.model';
 import { TSettings } from './settings.type';
 
-const get = async () => {
-  const settings = await Settings.findOne()
-    .populate([
-      'homepage.sliderImages',
-      'homepage.bannerImage1',
-      'homepage.bannerImage2',
-      'homepage.bannerImage3',
-      'homepage.featuredBrands',
-      'category.showOnTopMenu',
-      'category.featured',
-    ])
-    .populate([
-      {
-        path: 'homepage.featuredCategories',
-        populate: 'image',
-      },
-      {
-        path: 'homepage.popularProducts',
-        populate: {
-          path: 'images',
-        },
-      },
-      {
-        path: 'homepage.topSellingProducts',
-        populate: {
-          path: 'images',
-        },
-      },
-      {
-        path: 'homepage.trendingProducts',
-        populate: {
-          path: 'images',
-        },
-      },
-      {
-        path: 'homepage.featuredProducts',
-        populate: [
-          {
-            path: 'banner',
-          },
-          {
-            path: 'products',
-            populate: 'images',
-          },
-        ],
-      },
-      {
-        path: 'homepage.flashSale',
-        populate: {
-          path: 'product',
-          populate: 'images',
-        },
-      },
-    ]);
+const homePopulate1 = [
+  'homepage.sliderImages',
+  'homepage.bannerImage1',
+  'homepage.bannerImage2',
+  'homepage.bannerImage3',
+  'homepage.featuredBrands',
+];
 
-  return settings;
+const homePopulate2 = [
+  {
+    path: 'homepage.featuredCategories',
+    populate: 'image',
+  },
+  {
+    path: 'homepage.popularProducts',
+    populate: {
+      path: 'images',
+    },
+  },
+  {
+    path: 'homepage.topSellingProducts',
+    populate: {
+      path: 'images',
+    },
+  },
+  {
+    path: 'homepage.trendingProducts',
+    populate: {
+      path: 'images',
+    },
+  },
+  {
+    path: 'homepage.featuredProducts',
+    populate: [
+      {
+        path: 'banner',
+      },
+      {
+        path: 'products',
+        populate: 'images',
+      },
+    ],
+  },
+  {
+    path: 'homepage.flashSale',
+    populate: {
+      path: 'product',
+      populate: 'images',
+    },
+  },
+];
+
+const categoryPopulate = [
+  { path: 'category.showOnTopMenu', populate: 'subCategories' },
+  { path: 'category.featured', populate: 'subCategories' },
+];
+
+const get = async (type: string) => {
+  if (type === 'homepage') {
+    const settings = await Settings.findOne()
+      .populate(homePopulate1)
+      .populate(homePopulate2)
+      .select('homepage');
+
+    return settings;
+  } else if (type === 'category') {
+    const settings = await Settings.findOne()
+      .populate(categoryPopulate)
+      .select('category');
+
+    return settings;
+  } else if (type === 'footer') {
+    const settings = await Settings.findOne().select('footer');
+
+    return settings;
+  } else {
+    const settings = await Settings.findOne()
+      .populate([...homePopulate1, ...categoryPopulate])
+      .populate(homePopulate2);
+
+    return settings;
+  }
 };
 
 const update = async (payload: Partial<TSettings>) => {
