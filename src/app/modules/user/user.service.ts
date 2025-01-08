0d70b8301549c58b67abbeb7e9a5624f36d62aee
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-01-08 14:02:57 +0600

    feat: Implement getAll function in UserService for retrieving users with pagination and search

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,4 +1,6 @@
-import { USER_ROLE } from './user.constant';
+import { FilterQuery } from 'mongoose';
+import calculatePagination from '../../utils/calculatePagination';
+import { USER_ROLE, userRoles, userSearchableFields } from './user.constant';
 import User from './user.model';
 import { TUser } from './user.type';
 
@@ -6,6 +8,39 @@ const createAdmin = async (payload: TUser) => {
   await User.create({ ...payload, role: USER_ROLE.ADMIN });
 
   return null;
+};
+
+const getAll = async (filters: Record<string, any>) => {
+  const { page, limit, skip, sort, sortOrder } = calculatePagination(filters);
+
+  // handle search
+  const searchConditions = {
+    $or: userSearchableFields.map((field) => ({
+      [field]: { $regex: filters?.searchTerm ?? '', $options: 'i' },
+    })),
+  };
+
+  const query: FilterQuery<TUser> = searchConditions;
+
+  if (filters?.role && userRoles.includes(filters.role)) {
+    query.role = filters.role;
+  }
+
+  const users = await User.find(query)
+    .sort({ [sort]: sortOrder } as any)
+    .skip(skip)
+    .limit(limit);
+
+  const total = await User.countDocuments(query);
+
+  return {
+    data: users,
+    meta: {
+      page,
+      limit,
+      total,
+    },
+  };
 };
 
 const update = async (
@@ -19,5 +54,6 @@ const update = async (
 
 export const UserService = {
   createAdmin,
+  getAll,
   update,
 };
