d476911802d74de0af0d3358d843dceb694a63cc
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-10 22:42:04 +0600

    feat: Add 'test_admin' role to user roles and constants

--- src/app/modules/user/user.constant.ts
+++ src/app/modules/user/user.constant.ts
@@ -1,6 +1,11 @@
 import { TUserRole } from './user.type';
 
-export const userRoles: TUserRole[] = ['super_admin', 'admin', 'user'];
+export const userRoles: TUserRole[] = [
+  'super_admin',
+  'admin',
+  'user',
+  'test_admin',
+];
 
 export const userSearchableFields = ['name', 'email'];
 
@@ -8,4 +13,5 @@ export const USER_ROLE = {
   SUPER_ADMIN: 'super_admin',
   ADMIN: 'admin',
   USER: 'user',
+  TEST_ADMIN: 'test_admin',
 } as const;
